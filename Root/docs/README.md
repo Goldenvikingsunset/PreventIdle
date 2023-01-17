52f00a0b596fa1cbd32a382fa1d4e40fef41695a
GoldenVikingSunset <Renni@GoldenVikingSunset.onmicrosoft.com>
2023-01-17 23:57:37 +0000

    update changed to a typing test app

--- Root/docs/README.md
+++ Root/docs/README.md
@@ -1,31 +1,18 @@
-# PreventIdle
-PreventIdle is a script that simulates user activity to prevent the computer from going into idle mode. It can run indefinitely or for a specified number of repetitions.
+Typing Test
+A typing test application using the PyQt5 library.
 
-## Features
-- Simulates user activity by moving the mouse, changing the volume, simulating keyboard or screen saver
-- Allows the user to set the sleep time between runs
-- Allows the user to set the number of repetitions or run indefinitely
-- Allows the user to hide the window from the taskbar
-- Allows the user to choose which method to use
-- Allows the user to configure the parameters of the methods
-- Allows the user to pause and resume the script
-
-## Requirements
-- Python 3.x
-- PySimpleGUI
-- pyautogui
-
-## Installation
-1. Install Python 3.x
-2. Install the required libraries by running `pip install PySimpleGUI pyautogui` in the command line
-3. Download the script
-
-## Usage
-1. Run the script by running `python preventidle.py` in the command line
-2. Use the GUI to configure the options
-
-## Known Issues
-- None
-
-## Contributions
-Contributions are welcome. Feel free to open an issue or submit a pull request.
+Features
+Select from 3 pre-defined test strings or enter a custom text.
+Start and stop the test.
+Calculate total time taken, speed, accuracy, words per minute and score.
+Leaderboard list widget to store the scores.
+Instructions
+Select the text you want to use for the test from the combo box.
+Click the 'Start' button to start the test.
+Type the text as quickly and accurately as you can.
+Click the 'Stop' button to stop the test.
+Enter your name to add your score to the leaderboard.
+Notes
+The test will automatically stop after 60 seconds.
+The score is calculated based on speed, accuracy and words per minute.
+The leaderboard stores the highest scores.
--- Root/src/main.py
+++ Root/src/main.py
@@ -1,45 +1,105 @@
 import time
-import pyautogui
-import PySimpleGUI as sg
-import multiprocessing
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton, QDialog, QComboBox, QListWidget
+from PyQt5.QtCore import Qt, QTimer
 
+class TypingTest(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Typing Test")
 
-def KeepUI():
+        # create a combo box to select the test string
+        self.text_combo_box = QComboBox()
+        self.text_combo_box.addItems(["Text 1", "Text 2", "Text 3", "Custom Text"])
+        self.text_combo_box.activated.connect(self.select_text)
 
-    sg.theme('DarkAmber')
-    layout = [
-        [sg.Text('Make-Em-Think-You-Are-Working is now running.\nYou can keep it minimised, and it will continue running.\nClose it to disable it.', font=("Helvetica", 15), text_color='#FFD700')],
-        [sg.Button('Start', key='Start'), sg.Button('Stop', key='Stop', disabled=True)]
-    ]
+        # create a label to display the test string
+        self.text_label = QLabel()
+        self.text_label.setAlignment(Qt.AlignCenter)
 
-    window = sg.Window('Make-Em-Think-You-Are-Working', layout, alpha_channel=1,
-                       element_justification='c', background_color='#2F4F4F')
+        # create a line edit to accept user input
+        self.user_input = QLineEdit()
 
-    while True:
-        event, values = window.read()
-        if event == 'Start':
-            p2 = multiprocessing.Process(target=dontsleep)
-            p2.start()
-            window['Start'].update(disabled=True)
-            window['Stop'].update(disabled=False)
-        elif event == 'Stop':
-            p2.terminate()
-            window['Start'].update(disabled=False)
-            window['Stop'].update(disabled=True)
-        elif event == sg.WIN_CLOSED:  # if user closes window or clicks cancel
-            if p2.is_alive():
-                p2.terminate()
-            break
+        # create a start button
+        self.start_button = QPushButton("Start")
+        self.start_button.clicked.connect(self.start_test)
 
+        # create a stop button
+        self.stop_button = QPushButton("Stop")
+        self.stop_button.clicked.connect(self.stop_test)
+        self.stop_button.setEnabled(False)
 
-def dontsleep():
-    while True:
-        pyautogui.press('volumedown')
-        time.sleep(2)
-        pyautogui.press('volumeup')
-        time.sleep(500)
+        # create a layout to hold the text label, user input, and buttons
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.text_combo_box)
+        self.layout.addWidget(self.text_label)
+        self.layout.addWidget(self.user_input)
 
+        # create a layout to hold the start and stop buttons
+        self.buttons_layout = QHBoxLayout()
+        self.buttons_layout.addWidget(self.start_button)
+        self.buttons_layout.addWidget(self.stop_button)
+        self.layout.addLayout(self.buttons_layout)
+
+        self.setLayout(self.layout)
+
+        # create a timer
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_timer)
+
+        # create a leaderboard
+        self.leaderboard = QListWidget()
+        self.leaderboard.setFixedSize(200, 300)
+        self.layout.addWidget(self.leaderboard)
+        
+        self.texts = ["The quick brown fox jumps over the lazy dog. " * 5,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"]
+        self.select_text(0)
+
+    def select_text(self, index):
+        if index == 3:
+            self.text = input("Enter the text you want to use for the test:")
+        else:
+            self.text = self.texts[index]
+        self.text_label.setText(self.text)
+
+    def start_test(self):
+        self.start_time = time.time()
+        self.user_input.setReadOnly(False)
+        self.start_button.setEnabled(False)
+        self.stop_button.setEnabled(True)
+        self.timer.start(1000)
+        self.elapsed_time = 0
+        
+    def update_timer(self):
+        self.elapsed_time += 1
+        if self.elapsed_time == 60:
+            self.stop_test()
+        
+    def stop_test(self):
+        self.timer.stop()
+        self.end_time = time.time()
+        total_time = self.end_time - self.start_time
+        user_input_text = self.user_input.text()
+        speed = len(user_input_text) / total_time
+        accuracy = (len(self.text) - abs(len(self.text) - len(user_input_text))) / len(self.text) * 100
+        wpm = speed * 60 / 5
+        score = wpm * accuracy / 100
+
+        print("Time taken: {:.2f} seconds".format(total_time))
+        print("Speed: {:.2f} characters per second".format(speed))
+        print("Accuracy: {:.2f}%".format(accuracy))
+        print("Words per minute: {:.2f}".format(wpm))
+        print("Score: {:.2f}".format(score))
+        
+        #add the score and username to the leaderboard
+        name = input("Enter your name: ")
+        self.leaderboard.addItem(f"{name} - {score:.2f}")
+
+        self.user_input.setReadOnly(True)
+        self.start_button.setEnabled(True)
+        self.stop_button.setEnabled(False)
 
 if __name__ == '__main__':
-    p1 = multiprocessing.Process(target=KeepUI)
-    p1.start()
+    app = QApplication([])
+    typing_test = TypingTest()
+    typing_test.show()
+    app.exec_()
